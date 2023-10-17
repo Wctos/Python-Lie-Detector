e5d58eb26033a5523235fe0acfe69f6acb7eae67
Wctos <148076359+Wctos@users.noreply.github.com>
2023-10-17 17:31:37 +0700

    Script.py

---
+++ Script.py
@@ -0,0 +1,27 @@
+import time
+
+print("\__||'__'||__/ - This Tool Made By Wctos")
+
+Name = input(' > What is Your Name? : ')
+if Name == 'Target Name':
+    print("True")
+else:
+    print("False")
+
+time.sleep(2)
+
+Age = input(' > How old are you? : ')
+if Age == 'Target Age':
+    print("True")
+else:
+    print("False")
+
+time.sleep(2)
+
+print("Loading ...")
+
+time.sleep(5)
+
+print("Done!")
+while True:
+    Pram = input("> ")
